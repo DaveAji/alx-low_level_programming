9aa7e869aad725c75874a0e45042e9e0d59d2853
DaveAji <ajigbotoluwaod@gmail.com>
2023-06-21 16:28:46 -0700

    g

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,29 +8,15 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
-
 	for (i = 0; i < 24; i++)
 	{
-		for(j = 0; j < 60; j++)
+		for (j = 0; j < 60; j++)
 		{
-			if(i < 10)
-			{
-				_putchar('0' + i);
-			}
-			else
-			{
-				_putchar(i);
-			}
+			 _putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			if(j < 10)
-			{
-				_putchar('0' + j);	
-			}
-			else
-			{
-				_putchar(j);
-			}
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
