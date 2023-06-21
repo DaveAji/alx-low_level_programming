aea0c56551dd05230d003ad04efb828294c9016b
DaveAji <ajigbotoluwaod@gmail.com>
2023-06-21 15:38:40 -0700

    f

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,22 @@
  */
 int print_sign(int n)
 {
-	int a=0;
+	int a = 0;
+
 	if (n > 0)
 	{
-		_putchar('+');
-		a=1;
-	} else if (n == 0)
+	_putchar('+');
+	a = 1;
+	} 
+	else if (n == 0)
 	{
 	_putchar('0');
-	a=0;
+	a = 0;
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
-		_putchar('-');
-		a=-1;
+	_putchar('-');
+	a = -1;
 	}
 	return a;
 }
-
-
-     
