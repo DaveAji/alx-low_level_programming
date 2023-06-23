64523b03e3bd83747022d12b40f35c6793322f30
DaveAji <ajigbotoluwaod@gmail.com>
2023-06-22 18:27:43 -0700

    h

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -35,3 +35,21 @@ void print_remaining_days(int month, int day, int year)
         }
     }
 }
+bool checkYear(int year)
+{
+    // If a year is multiple of 400,
+    // then it is a leap year
+    if (year % 400 == 0)
+        return true;
+
+    // Else If a year is multiple of 100,
+    // then it is not a leap year
+    if (year % 100 == 0)
+        return false;
+
+    // Else If a year is multiple of 4,
+    // then it is a leap year
+    if (year % 4 == 0)
+        return true;
+    return false;
+}
